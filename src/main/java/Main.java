0c8834459e1b583563802101a567b6d1775248f5
S-Gauri-23 <76697678+S-Gauri-23@users.noreply.github.com>
2025-02-24 17:21:14 +0530

    completed type builtin; builtins

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,35 @@
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-//         Uncomment this block to pass the first stage
+
          Scanner scanner = new Scanner(System.in);
+        Set<String> commands = Set.of("echo", "exit", "type");
 
          while(true){
             System.out.print("$ ");
             String input = scanner.nextLine();
 
-            //checks if client wants to exit
+            // checks if client wants to exit
             if(input.equals("exit 0")) break;
 
-            //checks if client wants to print some thing
+            // checks if client wants to print some thing
             else if (input.startsWith("echo")) {
                 System.out.println(input.substring(5));
-            } else {
+            }
+
+            //
+            else if(input.startsWith("type")){
+                String inputCommand = input.substring(5);
+                if(commands.contains(inputCommand)){
+                    System.out.println(inputCommand + " is a shell builtin");
+                }
+                else{
+                    System.out.println(inputCommand + ": not found");
+                }
+            }
+            else {
                 System.out.println(input + ": command not found");
             }
          }
