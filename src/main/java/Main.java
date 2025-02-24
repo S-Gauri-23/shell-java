5c17edba86bbfc17d8914f6ac7b431af2e01a0bd
S-Gauri-23 <76697678+S-Gauri-23@users.noreply.github.com>
2025-02-24 12:55:31 +0530

    added a loop

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,9 +4,11 @@ public class Main {
     public static void main(String[] args) throws Exception {
 //         Uncomment this block to pass the first stage
          System.out.print("$ ");
+         Scanner scanner = new Scanner(System.in);
 
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        System.out.println(input + ": command not found");
+         while(true){
+            String input = scanner.nextLine();
+            System.out.println(input + ": command not found");
+         }
     }
 }
