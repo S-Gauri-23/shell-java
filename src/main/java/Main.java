8199ad337e9e7358ed10e0c11c54225c690d0423
S-Gauri-23 <76697678+S-Gauri-23@users.noreply.github.com>
2025-02-24 19:10:25 +0530

    completed The type builtin: executable files

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,37 +1,57 @@
+import java.nio.file.Path;
 import java.util.Scanner;
+import java.io.File;
 import java.util.Set;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
 
-         Scanner scanner = new Scanner(System.in);
-        Set<String> commands = Set.of("echo", "exit", "type");
+        Scanner scanner = new Scanner(System.in);
+        Set<String> commands = Set.of("echo", "exit", "type", "ls");
 
-         while(true){
+        while (true) {
             System.out.print("$ ");
             String input = scanner.nextLine();
 
             // checks if client wants to exit
-            if(input.equals("exit 0")) break;
+            if (input.equals("exit 0")) break;
 
-            // checks if client wants to print some thing
+                // checks if client wants to print some thing
             else if (input.startsWith("echo")) {
                 System.out.println(input.substring(5));
             }
 
-            //
-            else if(input.startsWith("type")){
+            // To handle type command
+            else if (input.startsWith("type")) {
                 String inputCommand = input.substring(5);
-                if(commands.contains(inputCommand)){
-                    System.out.println(inputCommand + " is a shell builtin");
-                }
-                else{
-                    System.out.println(inputCommand + ": not found");
+                if (commands.contains(inputCommand)) {
+                    String path = findCommandInPath(inputCommand);
+
+                    if (path != null) {
+                        System.out.println(inputCommand + " is " + path);
+                    } else {
+                        System.out.println(inputCommand + ": not found");
+                    }
+                } else {
+                    System.out.println(inputCommand + ": command not found");
                 }
             }
-            else {
-                System.out.println(input + ": command not found");
+        }
+    }
+
+    private static String findCommandInPath(String command) {
+        String pathEnv = System.getenv("PATH");
+        if (pathEnv == null || pathEnv.isEmpty()) {
+            return null;
+        }
+
+        String[] directories = pathEnv.split(";");
+        for (String dir : directories) {
+            File file = new File(dir, command);
+            if (file.exists() && file.canExecute()) {
+                return file.getAbsolutePath();
             }
-         }
+        }
+        return null;
     }
 }
