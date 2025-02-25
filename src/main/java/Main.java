c5fdde6598ad3f79f3443dc07496f6b03ed9c189
S-Gauri-23 <76697678+S-Gauri-23@users.noreply.github.com>
2025-02-25 17:24:46 +0530

    completed task run a program

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Scanner;
@@ -15,10 +16,10 @@ public class Main {
             String input = scanner.nextLine();
 
             // Split input into command and arguments
-            String[] tokens = input.split("\\s+", 2);
+            String[] tokens = input.split("\\s+");
             String command = tokens[0];
-            String argument = tokens.length > 1 ? tokens[1] : "";
-
+            String[] arguments = new String[tokens.length-1];
+            System.arraycopy(tokens, 1, arguments, 0, arguments.length);
 
             switch(command){
                 case "exit":{
@@ -27,29 +28,29 @@ public class Main {
                 }
 
                 case "echo":{
-                    System.out.println(argument);
+                    System.out.println(String.join(" ", arguments));
                     break;
                 }
 
                 case "type":{
-
-                        if(commands.contains(argument)){
-                            System.out.println(argument + " is a shell builtin");
-                            break;
+                    if(commands.contains(arguments[0])){
+                        System.out.println(arguments[0] + " is a shell builtin");
+                        break;
+                    }
+                    else{
+                        String path = getPath(arguments[0]);
+                        if (path != null) {
+                            System.out.println(arguments[0] + " is " + path);
+                        } 
+                        else {
+                            System.out.println(arguments[0] + ": not found");
                         }
-                        else{
-                            String path = getPath(argument);
-                            if (path != null) {
-                                System.out.println(argument + " is " + path);
-                            } 
-                            else {
-                                System.out.println(argument + ": not found");
-                            }
-                            break;
-                        }
+                        break;
+                    }
                 }
+
                 default:{
-                    System.out.println(command + ": command not found");
+                    executeCommand(command, arguments);
                     break;
                 }
             }
@@ -70,5 +71,27 @@ public class Main {
             }
         }
         return null;
-      }
+    }
+
+    private static void executeCommand(String command, String args[]){
+        try{
+            // construct full command with arguments
+            String[] fullCommand = new String[args.length + 1];
+            fullCommand[0] = command;
+            System.arraycopy(args, 0, fullCommand, 0, args.length);
+
+            // Execute the command
+            ProcessBuilder pb = new ProcessBuilder(fullCommand);
+            pb.inheritIO(); // Make sure the output is printed to the console
+
+            Process process = pb.start();
+            process.waitFor();  // Wait for command to finish execution
+        }
+        catch(IOException e){
+            System.out.println();
+        }
+        catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
 }
